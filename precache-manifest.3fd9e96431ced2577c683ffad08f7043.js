3230918b9dc9de7489a51c95fc3171722e621a84
JeremyE96 <elliottjeremy26@gmail.com>
2020-10-09 22:13:13 +0100

    Updates

---
+++ precache-manifest.3fd9e96431ced2577c683ffad08f7043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078fb07d97d4f0275c9ff2d58c465e0b",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "125267333f809e24ef55",
+    "url": "/react-to-do-list/static/css/main.b6bb3650.chunk.css"
+  },
+  {
+    "revision": "572c960d4eb2e5ccc0d5",
+    "url": "/react-to-do-list/static/js/2.12181699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-list/static/js/2.12181699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125267333f809e24ef55",
+    "url": "/react-to-do-list/static/js/main.942b45be.chunk.js"
+  },
+  {
+    "revision": "ec4e598f4a00753166e5",
+    "url": "/react-to-do-list/static/js/runtime-main.323e205d.js"
+  }
+]);
